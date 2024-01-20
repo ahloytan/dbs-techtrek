7d0ceda9e244b32e9398b2054f2c9757f2af8332
br234 <61417836+br234@users.noreply.github.com>
2024-01-20 13:34:53 +0800

    add itinerary card

--- frontend/src/components/ItineraryCard.js
+++ frontend/src/components/ItineraryCard.js
@@ -1,58 +1,56 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from '@mui/material';
+import { CardActionArea } from "@mui/material";
+
+const images = [
+  {
+    country_name: 1, // change to singapore
+    url: "https://a.cdn-hotels.com/gdcs/production8/d1098/064a4e00-23ee-4137-8ec3-a0d27bca0782.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+  },
+];
 
 const ItineraryCard = ({ itinerary }) => {
-    const handleClick = () => {
-        console.log('clicked on itinerarycard')
-    }
+  const handleClick = () => {
+    console.log("clicked on itinerarycard");
+  };
 
-    return (
-        <div>
-      <Card sx={{ maxWidth: 345, borderRadius: 2, margin: '10px' }} elevation={3}>
+  const image = images.filter((image) => image.country_name === itinerary.country_name)[0];
+
+  return (
+    <div>
+      <Card sx={{ maxWidth: 345, borderRadius: 1, margin: "10px" }} elevation={3}>
         <CardActionArea onClick={handleClick}>
-            <CardHeader
-            //   action={
-            //     <>
-            //       <IconButton onClick={() => handleDelete()}>
-            //         <DeleteOutlined />
-            //       </IconButton>
-
-            //       <IconButton
-            //         onClick={handleClick}
-            //       >
-            //         <EditOutlined />
-            //       </IconButton>
-            //       <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-            //         <MenuItem onClick={handleEditTitle}>Edit Title</MenuItem>
-            //         <MenuItem onClick={handleEditContent}>Edit Content</MenuItem>
-            //       </Menu>
-            //     </>
-            //   }
-            title={itinerary.title}
-            />
-            <CardMedia
-                component="img"
-                height="194"
-                image="https://a.cdn-hotels.com/gdcs/production8/d1098/064a4e00-23ee-4137-8ec3-a0d27bca0782.jpg?impolicy=fcrop&w=800&h=533&q=medium"
-                alt="image"
-            />
-            <CardContent>
-            <Typography variant="body1" color="text.secondary">
-                Country: {itinerary.country_name}
+          <CardHeader title={itinerary.title} />
+          <CardMedia component="img" height="194" image={image.url} alt="image" />
+          <CardContent>
+            <Typography variant="h6" color="text.secondary">
+              Country:
             </Typography>
             <Typography variant="body1" color="text.secondary">
-                Budget: {itinerary.budget}
+              {itinerary.country_name}
             </Typography>
-            </CardContent>
+            <Typography variant="h6" color="text.secondary">
+              Budget:
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              ${itinerary.budget}
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              <ul>
+                {itinerary.destinations && <Typography variant="h6">Destinations:</Typography>}
+                {itinerary.destinations &&
+                  itinerary.destinations.map((destination) => <li>{destination}</li>)}
+              </ul>
+            </Typography>
+          </CardContent>
         </CardActionArea>
       </Card>
     </div>
-    )
-}
+  );
+};
 
 export default ItineraryCard;
--- frontend/src/pages/itineraries.js
+++ frontend/src/pages/itineraries.js
@@ -1,57 +1,58 @@
-import { useEffect } from 'react'
-// import { usePostContext } from '../hooks/usePostContext'
-// import { useAuthContext } from '../hooks/useAuthContext'
-import Head from 'next/head';
-import { getItineraries } from '../api/index'
-import { Box, Grid } from '@mui/material';
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import ItineraryCard from '../components/ItineraryCard'
+import { useEffect } from "react";
+import Head from "next/head";
+import { getItineraries } from "../api/index";
+import { Box, Grid } from "@mui/material";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import ItineraryCard from "../components/ItineraryCard";
 // import { Container } from '@mui/material'
 // import Masonry from 'react-masonry-css';
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 
-const itinerariesList = [{'id': 1, 'country_name': 1, 'user_id': 1, 'budget': 500, 'title': 'Sightseeing in Singapore'},
-                    {'id': 2, 'country_name': 1, 'user_id': 1, 'budget': 800, 'title': 'Singapore Adventure'}]
+const itinerariesList = [
+  {
+    id: 1,
+    country_name: 1,
+    user_id: 1,
+    budget: 500,
+    title: "Sightseeing in Singapore",
+    destinations: ["MBS", "Singapore Zoo", "Chinatown"],
+  },
+  { id: 2, country_name: 1, user_id: 1, budget: 800, title: "Singapore Adventure" },
+];
 
 const Page = () => {
+  useEffect(() => {
+    const itineraries = getItineraries();
+  }, []);
 
-    useEffect(() => {
-        const itineraries = getItineraries()
-    }, [])
-
-    return (
-        <>
-            <Head>
-                <title>
-                Itineraries | DBS TechTrek
-                </title>
-            </Head>
-            <Box
-                component="main"
-                sx={{
-                    flexGrow: 1,
-                    py: 8
-                }}
-            >
-                <div style={{display: 'flex', width: '100%', marginBottom: '40px'}}>
-                    <Typography variant="h4">My Itineraries</Typography>
-                </div>
-                <Grid container spacing={2}>
-                    <Grid sx={{display: 'flex'}}>
-                            {itinerariesList && itinerariesList.map((itinerary) => (
-                                <ItineraryCard key={itinerary.id} itinerary={itinerary} />
-                            ))}
-                    </Grid>
-                </Grid>
-            </Box>
-        </>
-    )
-}
-
-Page.getLayout = (page) => (
-    <DashboardLayout>
-      {page}
-    </DashboardLayout>
+  return (
+    <>
+      <Head>
+        <title>Itineraries | DBS TechTrek</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <div style={{ display: "flex", width: "100%", marginBottom: "40px" }}>
+          <Typography variant="h4">My Itineraries</Typography>
+        </div>
+        <Grid container spacing={2}>
+          <Grid sx={{ display: "flex" }}>
+            {itinerariesList &&
+              itinerariesList.map((itinerary) => (
+                <ItineraryCard key={itinerary.id} itinerary={itinerary} />
+              ))}
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
-  
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
 export default Page;
