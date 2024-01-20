4dc3ff15402ddd0345361c007b1175d6a4323aab
CherylChee98 <90865808+CherylChee98@users.noreply.github.com>
2024-01-20 13:34:54 +0800

    Update itinerary.py
    API itineraries GET POST

---
+++ backend-flask/itinerary.py
@@ -0,0 +1,75 @@
+import pymysql 
+from flask import Flask 
+from db import mysql
+from app import app
+from flask import jsonify
+from flask import flash, request
+
+
+@app.route("/itineraries", methods = ['GET'])
+def get_itinerary():
+    try:
+        conn = mysql.connect()
+
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        user_id = request.args['user_id']
+        cursor.execute(f"SELECT itinerary.id, country.name  AS countryName, itinerary.budget, itinerary.title, destination.name \
+                       FROM itinerary INNER JOIN country INNER JOIN destination \
+                       ON itinerary.id = itinerary.country_id WHERE itinerary.user_id = {user_id}")
+        
+        row = cursor.fetchall()
+        if row != None:
+            resp = jsonify(row)
+        else:
+            resp = jsonify("itinerary not found")
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close()
+
+@app.route('/itineraries',methods = ['POST'])
+def insert_itineraries():
+    try:
+        _json = request.json
+        _country_id = _json['country_id']
+        _user_id = _json['user_id']
+        _budget = _json['budget']
+        _title = _json['title']
+   
+		# validate the received values
+        if _country_id and _user_id and _budget and _title and request.method == 'POST':
+            sql = 'INSERT INTO itinerary (id, country_id, user_id,budget,title) VALUES (%s,%s,%s,%s)'
+            data = (_country_id, _user_id,_budget,_title,)
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute(sql, data)
+            conn.commit()
+            resp = jsonify('Itinerary added successfully!')
+            resp.status_code = 200
+            return resp
+        else:
+            return not_found()
+    finally:
+        cursor.close()
+        conn.close()
+
+#@app.route('/itineraries/:id',methods = ['GET'])
+#def get_itineraries():
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(port = 8000, host="0.0.0.0")
