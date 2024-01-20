b33cdc41a7b25ba2c6f9753692cb0a1b2ea1856c
CherylChee98 <90865808+CherylChee98@users.noreply.github.com>
2024-01-20 10:07:59 +0800

    Create db.py
    database configuration

---
+++ backend-flask/db.py
@@ -0,0 +1,13 @@
+from app import app
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ""
+app.config['MYSQL_DATABASE_DB'] = 'bank'
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_PORT'] = 3308
+
+mysql.init_app(app)
