b2cc7ee5ccf22a2432f454f333d68e99c2527839
Aloysius <chewinggum222@hotmail.com>
2024-03-20 23:37:20 +0800

    implementing RBAC using supabase hooks

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,11 +1,18 @@
 import { NextResponse } from 'next/server'
 import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode"; 
+
 // This function can be marked `async` if using `await` inside
 export function middleware(req) {
   const hasJWT = cookies().get("jwt");
-  if (!hasJWT) return NextResponse.redirect(new URL('/auth/login', req.url));
+  const { app_metadata: { role_id }} = jwtDecode(hasJWT?.value);
+
+  if (!hasJWT || role_id !== 1) return NextResponse.redirect(new URL('/auth/login', req.url));
 }
  
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|auth|assets|).*)']
+  matcher: [
+    '/account',
+    '/((?!_next/static|_next/image|favicon.ico|auth|assets|).*)'
+  ]
 }
