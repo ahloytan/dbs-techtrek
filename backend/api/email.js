3eb030924d92ad00ce42cc3a55f2190861d8191d
Aloysius <chewinggum222@hotmail.com>
2025-05-21 21:18:54 +0800

    updated email content

--- backend/api/email.js
+++ backend/api/email.js
@@ -12,19 +12,19 @@ router.post('/', async function (req, res, next) {
 
     const data = await resend.emails.send({
         from: 'Ahloy <no-reply@spotonenglishschool.net>',
-        to: ['jackcobbett12@gmail.com','aloysius.dmit@gmail.com','chewinggum222@hotmail.com'],
-        subject: 'WEBSITE CONTACT FORM',
+        to: ['spotonenglish@gmail.com','aloysius.dmit@gmail.com'],
+        subject: '体験申し込み依頼がありました',
         html: `
-            <p>Parent Name (Kanji): <b>${parentNameKanji}</b></p>
-            <p>Parent Name (Hiragana): <b>${parentNameHiragana}</b></p>
-            <p>Student Name (Kanji): <b>${studentNameKanji}</b></p>
-            <p>Student Name (Hiragana): <b>${studentNameHiragana}</b></p>
-            <p>Current Address: <b>${address}</b></p>
-            <p>Email Address: <b>${email}</b></p>
-            <p>Phone Number: <b>${phone}</b></p>
-            <p>Best time to call: <b>${bestTimeToCall}</b></p>
-            <p>Selected Class: <b>${classes.join(', ')}</b></p>
-            <p>Anything else: <b>${comments}</b></p>
+            <p>親の名前 (漢字): <b>${parentNameKanji}</b></p>
+            <p>親の名前 (ひらがな): <b>${parentNameHiragana}</b></p>
+            <p>生徒の名前 (漢字): <b>${studentNameKanji}</b></p>
+            <p>生徒の名前 (ひらがな): <b>${studentNameHiragana}</b></p>
+            <p>住所: <b>${address}</b></p>
+            <p>メールアドレス: <b>${email}</b></p>
+            <p>電話番号: <b>${phone}</b></p>
+            <p>電話連絡希望時間帯: <b>${bestTimeToCall}</b></p>
+            <p>ご希望のクラス: <b>${classes.join(', ')}</b></p>
+            <p>その他: <b>${comments}</b></p>
             `
     });
     console.log(req.body);
